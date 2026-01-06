343c6952971f624606e12f9d8b6923828a1cb6ea
dcampanini <campanini.diego@gmail.com>
2026-01-06 18:08:19 -0300

    minor updates

--- run_main_inference.sh
+++ run_main_inference.sh
@@ -3,11 +3,11 @@
 #SBATCH -t 3-00:00:00
 #SBATCH -p batch                                # Choose partition (interactive or batch)
 #SBATCH -q batch                                # Choose QoS, must be same as partition
-#SBATCH --cpus-per-task 16                    # Request 2 cores
-#SBATCH --mem=30G                                # Indicate required memory
+#SBATCH --cpus-per-task 4                    # Request 2 cores
+#SBATCH --mem=10G                                # Indicate required memory
 #SBATCH --gpus=1                                # Ask for 1 GPU
 #SBATCH --nodelist ih-condor
 module load conda
 conda activate /mnt/researchers/pablo-estevez/datasets/dcampanini/envs/env_sslprostate3
 # inference
-python main.py configs/unetr/unetr_prostate_p158_f0_inference.yaml
+python main.py configs/unetr/unetr_prostate_p158f0_inference.yaml
--- run_main_stage3.sh
+++ run_main_stage3.sh
@@ -3,15 +3,15 @@
 #SBATCH -t 3-00:00:00
 #SBATCH -p batch                                # Choose partition (interactive or batch)
 #SBATCH -q batch                                # Choose QoS, must be same as partition
-#SBATCH --cpus-per-task 16                    # Request 2 cores
+#SBATCH --cpus-per-task 8                    # Request 2 cores
 #SBATCH --mem=30G                                # Indicate required memory
 #SBATCH --gpus=1                                # Ask for 1 GPU
 #SBATCH --nodelist ih-condor
 module load conda
 conda activate /mnt/researchers/pablo-estevez/datasets/dcampanini/envs/env_sslprostate3
-#python main.py configs/unetr_prostate_p158f0.yaml &
-# python main.py configs/unetr_prostate_p158f1.yaml &
-# python main.py configs/unetr_prostate_p158f2.yaml 
-python main.py configs/unetr_prostate_p158f3.yaml &
-python main.py configs/unetr_prostate_p158f4.yaml
+# python main.py configs/unetr/unetr_prostate_p158f0.yaml &
+# python main.py configs/unetr/unetr_prostate_p158f1.yaml 
+# python main.py configs/unetr/unetr_prostate_p158f2.yaml &
+# python main.py configs/unetr/unetr_prostate_p158f3.yaml 
+python main.py configs/unetr/unetr_prostate_p158f4.yaml
 
