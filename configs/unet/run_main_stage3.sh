de33908ce15adb5688caec2e2b17ca8c1a0212a4
dcampanini <campanini.diego@gmail.com>
2026-01-14 00:33:30 -0300

    change to run unet, unetr

--- configs/unet/run_main_stage3.sh
+++ configs/unet/run_main_stage3.sh
@@ -3,30 +3,24 @@
 #SBATCH -t 3-00:00:00
 #SBATCH -p batch                                # Choose partition (interactive or batch)
 #SBATCH -q batch                                # Choose QoS, must be same as partition
-#SBATCH --cpus-per-task 16                    # Request 2 cores
+#SBATCH --cpus-per-task 8                    # Request 2 cores
 #SBATCH --mem=64G                                # Indicate required memory
 #SBATCH --gpus=1                                # Ask for 1 GPU
-#SBATCH --nodelist ih-condor
+#SBATCH --nodelist ih-loica
 module load conda
 conda activate /mnt/researchers/pablo-estevez/datasets/dcampanini/envs/env_sslprostate3
-# unetr p158
 # exec -a f0 python main.py configs/unetr/unetr_prostate_p158f0.yaml &
 # exec -a f1 python main.py configs/unetr/unetr_prostate_p158f1.yaml &
 # exec -a f2 python main.py configs/unetr/unetr_prostate_p158f2.yaml &
 # exec -a f3 python main.py configs/unetr/unetr_prostate_p158f3.yaml &
 # exec -a f4 python main.py configs/unetr/unetr_prostate_p158f4.yaml &
 # wait
-# unet uc
-# exec -a f0 python main.py configs/unet/unet_prostate_ucf0.yaml &
-# exec -a f1 python main.py configs/unet/unet_prostate_ucf1.yaml &
-# exec -a f2 python main.py configs/unet/unet_prostate_ucf2.yaml &
-# exec -a f3 python main.py configs/unet/unet_prostate_ucf3.yaml &
-# exec -a f4 python main.py configs/unet/unet_prostate_ucf4.yaml &
-# unet p158
-#exec -a f0 python main.py configs/unet/unet_prostate_p158f0.yaml #&
-#exec -a f1 python main.py configs/unet/unet_prostate_p158f1.yaml #&
-#exec -a f2 python main.py configs/unet/unet_prostate_p158f2.yaml #&
-#exec -a f3 python main.py configs/unet/unet_prostate_p158f3.yaml #&
-exec -a f4 python main.py configs/unet/unet_prostate_p158f4.yaml #&
-#wait
+
+
+exec -a f0 python main.py configs/unet/unetr_prostate_p158f0.yaml &
+exec -a f1 python main.py configs/unet/unetr_prostate_p158f1.yaml &
+exec -a f2 python main.py configs/unet/unetr_prostate_p158f2.yaml &
+exec -a f3 python main.py configs/unet/unetr_prostate_p158f3.yaml &
+exec -a f4 python main.py configs/unet/unetr_prostate_p158f4.yaml &
+wait
 
--- run_main_stage2.sh
+++ run_main_stage2.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name stage2-prostate          # Custom name
+#SBATCH --job-name stage2-ssl         # Custom name
 #SBATCH -t 3-00:00:00
 #SBATCH -p batch                                # Choose partition (interactive or batch)
 #SBATCH -q batch                                # Choose QoS, must be same as partition
 #SBATCH --cpus-per-task 16                    # Request 2 cores
-#SBATCH --mem=30G                                # Indicate required memory
+#SBATCH --mem=48G                                # Indicate required memory
 #SBATCH --gpus=1                                # Ask for 1 GPU
 #SBATCH --nodelist ih-condor
 module load conda
 conda activate /mnt/researchers/pablo-estevez/datasets/dcampanini/envs/env_sslprostate3
-python main.py configs/unetr_prostate_picai_f0.yaml
+exec -a stage2  python main.py configs/unetr/unetr_stage2_picai_f0.yaml
+#exec -a stage2_only  python main.py configs/unetr/unetr_stage2_only_picai_f0.yaml
 
